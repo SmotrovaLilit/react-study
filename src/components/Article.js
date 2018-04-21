eb7f437dac3d852972bb3dcd6047b2fe0328c1bb
lilit <lilit2990@gmail.com>
2018-04-21 18:59:13 +0300

    5. Интерактивность, состояние компонентов

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function Article(props) {
-    const {article} = props
-    return (
-        <div>
-            <h2>{article.title}</h2>
-            <section>{article.text}</section>
-            <div>{(new Date(article.date).toDateString())}</div>
-        </div>
-    )
+
+class Article extends Component {
+    state = {
+        isOpen: true
+    };
+
+    render() {
+        const {article} = this.props;
+
+        return (
+            <div>
+                <h2>
+                    {article.title}
+                    <button onClick={this.handleClick}>
+                        { this.state.isOpen ? 'close' : 'open'}
+                    </button>
+                </h2>
+                <section>{this.state.isOpen && article.text}</section>
+                <div>{(new Date(article.date).toDateString())}</div>
+            </div>
+        );
+    }
+
+    handleClick = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
 }
-
 
 export default Article;
