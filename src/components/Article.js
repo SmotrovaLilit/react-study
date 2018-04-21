79a198b92eab16727d78ef8585e4ae0bb4ad52e8
lilit <lilit2990@gmail.com>
2018-04-21 19:33:47 +0300

    8. Оформление компонентов, работа с CSS

--- src/components/Article.js
+++ src/components/Article.js
@@ -5,22 +5,26 @@ class Article extends Component {
     state = {
         isOpen: true
     };
-
     render() {
         const {article} = this.props;
-
         return (
-            <div>
-                <h2>
-                    {article.title}
-                    <button onClick={this.handleClick}>
-                        { this.state.isOpen ? 'close' : 'open'}
-                    </button>
-                </h2>
-                <section>{this.state.isOpen && article.text}</section>
-                <div>{(new Date(article.date).toDateString())}</div>
+            <div className="card mx-auto" style={{width:'50%'}}>
+                <div className="card-header">
+                    <h2>
+                        {article.title}
+                        <button className="btn btn-primary btn-lg float-right" onClick={this.handleClick}>
+                            {this.state.isOpen ? 'close' : 'open'}
+                        </button>
+                    </h2>
+                </div>
+                <div className="card-body">
+                    <div className="card-subtitle">{(new Date(article.date).toDateString())}</div>
+                    <section className="card-text">{this.state.isOpen && article.text}</section>
+                </div>
             </div>
         );
+
+
     }
 
     handleClick = () => {
