186cbf1873eeccc120c2d5f40c1bf49a080dd3e5
lilit <lilit2990@gmail.com>
2018-04-24 01:56:51 +0300

    12. Обратный поток данных, подъём состояния

--- src/components/Article.js
+++ src/components/Article.js
@@ -5,43 +5,43 @@ class Article extends PureComponent {
   constructor(props) {
     super(props)
     this.state = {
-      isOpen: props.defaultOpened,
       count:0
     }
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (this.props.defaultOpened !== nextProps.defaultOpened) {
-      this.setState({
-        isOpen: nextProps.defaultOpened
-      })
-    }
-  }
+  // componentWillReceiveProps(nextProps) {
+  //   if (this.props.defaultOpened !== nextProps.defaultOpened) {
+  //     this.setState({
+  //       isOpen: nextProps.defaultOpened
+  //     })
+  //   }
+  // }
 
   // shouldComponentUpdate(nextProps, nextState) {
   //   return this.state.isOpen !== nextState.isOpen
   // }
+
 
   componentWillUpdate() {
     console.log("component will update")
   }
 
   render() {
-    const {article} = this.props;
+    const {article, isOpen, onButtonClick} = this.props;
     return (
       <div className="card mx-auto" style={{width: '50%'}}>
         <div className="card-header">
           <h2 onClick={this.incrementCount}>
             {article.title}
             <span>{ this.state.count }</span>
-            <button className="btn btn-primary btn-lg float-right" onClick={this.handleClick}>
-              {this.state.isOpen ? 'close' : 'open'}
+            <button className="btn btn-primary btn-lg float-right" onClick={onButtonClick}>
+              {isOpen ? 'close' : 'open'}
             </button>
           </h2>
         </div>
         <div className="card-body">
           <div className="card-subtitle">{(new Date(article.date).toDateString())}</div>
-          <section className="card-text">{this.state.isOpen && article.text}</section>
+          <section className="card-text">{isOpen && article.text}</section>
         </div>
       </div>
     );
@@ -56,9 +56,9 @@ class Article extends PureComponent {
   }
 
   handleClick = () => {
-    this.setState({
-      isOpen: !this.state.isOpen
-    })
+    // this.setState({
+    //   isOpen: !this.state.isOpen
+    // })
   }
 
 }
--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Article from '../Article'
 import './style.css'
 
-export default function ArticleList({articles}) {
-    const articleElements = articles.map((article, i) =>
-        <li className="article-list__li" key={article.id}>
-          <Article article={article} defaultOpened={i === 0} />
-        </li>
-    );
+export default class ArticleList extends Component {
+  state = {
+    openArticleID: null
+  }
 
+  render() {
+    const {articles} = this.props
+    const articleElements = articles.map((article, i) =>
+      <li className="article-list__li" key={article.id}>
+        <Article
+          article={article}
+          isOpen={article.id === this.state.openArticleID}
+          onButtonClick={() => this.handleClick(article.id)}
+        />
+      </li>
+    )
     return (
-     <ul>
-         {articleElements}
-     </ul>
+      <ul>
+        {articleElements}
+      </ul>
     );
+  }
+
+  handleClick = openArticleID  => {
+    this.setState({
+      openArticleID: this.state.openArticleID === openArticleID ? null : openArticleID
+    })
+  }
 }
